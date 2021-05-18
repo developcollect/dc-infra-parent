2b25e18096196245851e83228463981b92e1e0ac
lanjingl <lanjingl@outlook.com>
2021-05-18 13:39:50 +0800

    1

--- dc-infra-core/src/main/java/com/developcollect/core/geo/Point.java
+++ dc-infra-core/src/main/java/com/developcollect/core/geo/Point.java
@@ -1,13 +1,25 @@
 package com.developcollect.core.geo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 坐标点
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Point {
 
     private double lat;
     private double lng;
+
+    public static Point of(double lat, double lng) {
+        return new Point(lat, lng);
+    }
+
+    public static Point of(int lat, int lng) {
+        return of((double) lat, (double) lng);
+    }
 }
