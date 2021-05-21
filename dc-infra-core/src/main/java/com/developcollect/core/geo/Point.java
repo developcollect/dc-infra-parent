6c09183e2c1af1140c9ccf90b92dbe481f37667e
zak <lanjingl@outlook.com>
2021-05-21 23:31:17 +0800

    1

--- dc-infra-core/src/main/java/com/developcollect/core/geo/Point.java
+++ dc-infra-core/src/main/java/com/developcollect/core/geo/Point.java
@@ -12,14 +12,34 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Point {
 
-    private double lat;
+    /**
+     * 经度
+     */
     private double lng;
+    /**
+     * 纬度
+     */
+    private double lat;
 
-    public static Point of(double lat, double lng) {
-        return new Point(lat, lng);
+    public static Point of(double lng, double lat) {
+        return new Point(lng, lat);
     }
 
-    public static Point of(int lat, int lng) {
-        return of((double) lat, (double) lng);
+    public static Point of(int lng, int lat) {
+        return of((double) lng, lat);
+    }
+
+    /**
+     * 解析经纬度，115.332,28.3455
+     * 前面是经度(lng)，后面是纬度(lat)
+     * @param str 经纬度
+     * @return point
+     */
+    public static Point of(String str) {
+        String[] split = str.split(", *");
+        if (split.length != 2) {
+            throw new IllegalArgumentException("无法解析坐标: " + str);
+        }
+        return of(Double.parseDouble(split[0]), Double.parseDouble(split[1]));
     }
 }
