869069a503dff64ece711a4356d7d6a73d4ea454
zak <lanjingl@outlook.com>
2021-06-03 09:59:56 +0800

    1

--- dc-infra-web/dc-infra-web-ssm/src/main/java/com/developcollect/web/ssm/service/IService.java
+++ dc-infra-web/dc-infra-web-ssm/src/main/java/com/developcollect/web/ssm/service/IService.java
@@ -1,4 +1,47 @@
 package com.developcollect.web.ssm.service;
 
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+
+import java.io.Serializable;
+
 public interface IService<T> extends com.baomidou.mybatisplus.extension.service.IService<T> {
+
+    <V> T getByField(SFunction<T, V> fieldGetter, V val);
+
+    boolean exist(Serializable id);
+
+    <V> boolean exist(SFunction<T, V> function, V val);
+
+    boolean exist(Wrapper<T> queryWrapper);
+
+    /**
+     * 检查指定属性值是否指定的id所有
+     *
+     * @param id          id
+     * @param fieldGetter
+     * @param val
+     * @param <V>
+     */
+    <V> boolean checkAttrOwner(Serializable id, SFunction<T, V> fieldGetter, V val);
+
+    /**
+     * 检查指定属性值在指定的id下是否可用
+     * 可用有两种情况：
+     * 1. 属性值本来就属于该id
+     * 2. 属性值无主
+     * <p>
+     * 例子:
+     * 判断用户名“张三”在id 123 下是否可用
+     * 如果id 123的用户名本来就是“张三”， 那么就可用的
+     * 如果用户名“张三”没用任何一个用户使用，那么也是可用的
+     *
+     * @param id          id
+     * @param fieldGetter 属性值方法
+     * @param val         属性值
+     * @param <V>         属性值泛型
+     * @return 是否可用
+     */
+    <V> boolean checkAttrAvailable(Serializable id, SFunction<T, V> fieldGetter, V val);
 }
--- dc-infra-web/dc-infra-web-ssm/src/main/java/com/developcollect/web/ssm/service/ServiceImpl.java
+++ dc-infra-web/dc-infra-web-ssm/src/main/java/com/developcollect/web/ssm/service/ServiceImpl.java
@@ -1,8 +1,19 @@
 package com.developcollect.web.ssm.service;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.baomidou.mybatisplus.core.toolkit.ReflectionKit;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.developcollect.core.utils.LambdaUtil;
 
-public class ServiceImpl<M extends BaseMapper<T>, T> extends com.baomidou.mybatisplus.extension.service.impl.ServiceImpl<M, T> {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class ServiceImpl<M extends BaseMapper<T>, T> extends com.baomidou.mybatisplus.extension.service.impl.ServiceImpl<M, T> implements IService<T> {
 
     @Override
     protected Class<T> currentMapperClass() {
@@ -12,5 +23,47 @@ public class ServiceImpl<M extends BaseMapper<T>, T> extends com.baomidou.mybati
     @Override
     protected Class<T> currentModelClass() {
         return (Class<T>) this.getResolvableType().as(ServiceImpl.class).getGeneric(1).getType();
+    }
+
+    @Override
+    public <V> T getByField(SFunction<T, V> fieldGetter, V val) {
+        return getOne(Wrappers.<T>query().eq(LambdaUtil.getFieldName(fieldGetter), val));
+    }
+
+    @Override
+    public boolean exist(Serializable id) {
+        return exist(Wrappers.<T>query().eq("id", id));
+    }
+
+    @Override
+    public <V> boolean exist(SFunction<T, V> function, V val) {
+        return exist(Wrappers.<T>query().eq(LambdaUtil.getFieldName(function), val));
+    }
+
+    @Override
+    public boolean exist(Wrapper<T> queryWrapper) {
+        return getBaseMapper().selectCount(queryWrapper) > 0;
+    }
+
+    @Override
+    public <V> boolean checkAttrOwner(Serializable id, SFunction<T, V> fieldGetter, V val) {
+        T entity = getById(id);
+        return entity != null && Objects.equals(fieldGetter.apply(entity), val);
+    }
+
+    @Override
+    public <V> boolean checkAttrAvailable(Serializable id, SFunction<T, V> fieldGetter, V val) {
+        T entity = getByField(fieldGetter, val);
+        return entity == null || Objects.equals(getId(entity), id);
+    }
+
+
+    protected Serializable getId(T entity) {
+        TableInfo tableInfo = TableInfoHelper.getTableInfo(this.entityClass);
+        Assert.notNull(tableInfo, "error: can not execute. because can not find cache of TableInfo for entity!");
+        String keyProperty = tableInfo.getKeyProperty();
+        Assert.notEmpty(keyProperty, "error: can not execute. because can not find column for id from entity!");
+        Serializable idVal = (Serializable) ReflectionKit.getFieldValue(entity, tableInfo.getKeyProperty());
+        return idVal;
     }
 }
