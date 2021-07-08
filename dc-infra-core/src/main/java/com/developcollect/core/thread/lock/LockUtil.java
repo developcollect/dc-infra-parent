42f4d250ce8bb3e1dbe481f9ca70da04d1fe9600
zak <lanjingl@outlook.com>
2021-07-08 23:39:53 +0800

    fix: lockUtil使用synchronized实现

--- dc-infra-core/src/main/java/com/developcollect/core/thread/lock/LockUtil.java
+++ dc-infra-core/src/main/java/com/developcollect/core/thread/lock/LockUtil.java
@@ -1,12 +1,12 @@
 package com.developcollect.core.thread.lock;
 
 
-import cn.hutool.core.collection.ConcurrentHashSet;
+
+import com.developcollect.core.utils.LambdaUtil;
 
 import java.util.Map;
-import java.util.Set;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author Zhu KaiXiao
@@ -15,19 +15,23 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class LockUtil extends cn.hutool.core.thread.lock.LockUtil {
 
-    private static final Map<Object, ReentrantLock> lockMap = new ConcurrentHashMap<>();
-    private static final ReentrantLock lock = new ReentrantLock();
+    private static final Map<Object, Thread> threadMap = new ConcurrentHashMap<>();
+    private static final Map<Object, Integer> lockCountMap = new ConcurrentHashMap<>();
 
     /**
      * 尝试获取锁并锁定，如果获取锁成功，返回true
      */
     public static boolean tryLock(Object obj) {
-        lock.lock();
-        try {
-            ReentrantLock lockForObj = lockMap.computeIfAbsent(obj, o -> new ReentrantLock());
-            return lockForObj.tryLock();
-        } finally {
-            lock.unlock();
+        synchronized (obj) {
+            boolean locked;
+            Thread currentThread = Thread.currentThread();
+            Thread thread = threadMap.computeIfAbsent(obj, k -> currentThread);
+            locked = thread == currentThread;
+            if (locked) {
+                Integer count = Optional.ofNullable(lockCountMap.get(obj)).orElse(0);
+                lockCountMap.put(obj, count + 1);
+            }
+            return locked;
         }
     }
 
@@ -35,33 +39,53 @@ public class LockUtil extends cn.hutool.core.thread.lock.LockUtil {
      * 获取锁，如果锁被占用则一直等待
      */
     public static void lock(Object obj) {
-        ReentrantLock lockForObj;
-        lock.lock();
-        try {
-            lockForObj = lockMap.computeIfAbsent(obj, o -> new ReentrantLock());
-        } finally {
-            lock.unlock();
-        }
+        boolean locked;
 
-        lockForObj.lock();
+        synchronized (obj) {
+            do {
+                Thread currentThread = Thread.currentThread();
+                Thread thread = threadMap.computeIfAbsent(obj, k -> currentThread);
+                locked = thread == currentThread;
+                if (locked) {
+                    Integer count = Optional.ofNullable(lockCountMap.get(obj)).orElse(0);
+                    lockCountMap.put(obj, count + 1);
+                } else {
+                    try {
+                        // 等待锁释放后被通知
+                        obj.wait();
+                    } catch (InterruptedException e) {
+                        LambdaUtil.raise(e);
+                    }
+                }
+            } while (!locked);
+        }
     }
 
     /**
      * 释放锁
      */
     public static void unlock(Object obj) {
-        lock.lock();
-        try {
-            ReentrantLock lockForObj = lockMap.get(obj);
-            if (lockForObj == null) {
+        synchronized (obj) {
+            Thread thread = threadMap.get(obj);
+            if (thread == null) {
                 throw new IllegalArgumentException("释放锁前需要先上锁：" + obj);
             }
-            lockForObj.unlock();
-            if (!lockForObj.hasQueuedThreads() && !lockForObj.isLocked()) {
-                lockMap.remove(obj);
+            Thread currentThread = Thread.currentThread();
+            if (thread == currentThread) {
+                Integer count = lockCountMap.get(obj) - 1;
+                if (count == 0) {
+                    // 重入次数归零了，说明锁释放了
+                    threadMap.remove(obj);
+                    lockCountMap.remove(obj);
+                    // 通知随机(HotSpot虚拟机的实现其实是取第一个)一个等待锁的线程争抢锁
+                    obj.notify();
+                } else {
+                    // 这里只是把重入次数-1，其实锁还是占用着的
+                    lockCountMap.put(obj, count);
+                }
+            } else {
+                throw new IllegalMonitorStateException("当前线程并不持有锁，无法执行释放锁操作");
             }
-        } finally {
-            lock.unlock();
         }
     }
 
--- dc-infra-core/src/test/java/com/developcollect/core/thread/lock/LockUtilTest.java
+++ dc-infra-core/src/test/java/com/developcollect/core/thread/lock/LockUtilTest.java
@@ -1,10 +1,13 @@
 package com.developcollect.core.thread.lock;
 
 import cn.hutool.core.thread.ThreadUtil;
+import com.developcollect.core.lang.SystemClock;
 import com.developcollect.core.utils.StrUtil;
 import org.junit.Test;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -16,29 +19,41 @@ public class LockUtilTest {
     public void testLock() {
         String key = "123";
 
+        int threadNum = 3;
+        CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(threadNum);
 
         Runnable runnable = () -> {
             String name = Thread.currentThread().getName();
             System.out.println(StrUtil.format("{}: 尝试上锁", name));
+            try {
+                // 等待其他线程，只有所有线程同时到达这里之后才会继续往下走
+                cyclicBarrier.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             LockUtil.lock(key);
+            System.out.println();
             System.out.println(StrUtil.format("{}: 上锁成功", name));
 
             ThreadUtil.sleep(2000);
 
             LockUtil.unlock(key);
             System.out.println(StrUtil.format("{}: 释放锁", name));
+            countDownLatch.countDown();
         };
 
-        Thread t1 = new Thread(runnable);
-        Thread t2 = new Thread(runnable);
-        t1.setName("t1");
-        t2.setName("t2");
-        t1.start();
-        t2.start();
+        for (int i = 0; i < threadNum; i++) {
+            Thread t = new Thread(runnable);
+            t.setName("T" + i);
+            t.start();
+        }
 
 
-        while (true) {
-            ThreadUtil.sleep(500);
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
@@ -47,9 +62,19 @@ public class LockUtilTest {
     public void testTryLock() {
         String key = "123";
 
+        int threadNum = 3;
+        CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(threadNum);
+
         Runnable runnable = () -> {
             String name = Thread.currentThread().getName();
             boolean locked;
+            try {
+                // 等待其他线程，只有所有线程同时到达这里之后才会继续往下走
+                cyclicBarrier.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             do {
                 locked = LockUtil.tryLock(key);
                 System.out.println(StrUtil.format("{}: 尝试上锁：{}", name, locked));
@@ -60,19 +85,23 @@ public class LockUtilTest {
 
             LockUtil.unlock(key);
             System.out.println(StrUtil.format("{}: 释放锁", name));
+            System.out.println();
+            countDownLatch.countDown();
         };
 
 
-        Thread t1 = new Thread(runnable);
-        Thread t2 = new Thread(runnable);
-        t1.setName("t1");
-        t2.setName("t2");
-        t1.start();
-        t2.start();
+        for (int i = 0; i < threadNum; i++) {
+            Thread t = new Thread(runnable);
+            t.setName("T" + i);
+            t.start();
+        }
 
 
-        while (true) {
-            ThreadUtil.sleep(500);
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
@@ -82,13 +111,21 @@ public class LockUtilTest {
 
         String key = "123";
 
-        CountDownLatch countDownLatch = new CountDownLatch(2);
-
+        int threadNum = 3;
+        CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(threadNum);
 
         Runnable runnable = () -> {
             String name = Thread.currentThread().getName();
             System.out.println(StrUtil.format("{}: 尝试上锁", name));
+            try {
+                // 等待其他线程，只有所有线程同时到达这里之后才会继续往下走
+                cyclicBarrier.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             LockUtil.lock(key);
+            System.out.println();
             System.out.println(StrUtil.format("{}: 上锁成功", name));
             LockUtil.lock(key);
             System.out.println(StrUtil.format("{}: 重入上锁成功", name));
@@ -102,12 +139,11 @@ public class LockUtilTest {
             countDownLatch.countDown();
         };
 
-        Thread t1 = new Thread(runnable);
-        Thread t2 = new Thread(runnable);
-        t1.setName("t1");
-        t2.setName("t2");
-        t1.start();
-        t2.start();
+        for (int i = 0; i < threadNum; i++) {
+            Thread t = new Thread(runnable);
+            t.setName("T" + i);
+            t.start();
+        }
 
 
         try {
@@ -115,6 +151,59 @@ public class LockUtilTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+    }
+
+
+    @Test
+    public void test_wait() {
+        String key = "123";
+
+        int threadNum = 3;
+        CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(threadNum);
+
+        Runnable runnable = () -> {
+            String name = Thread.currentThread().getName();
+            try {
+                // 等待其他线程，只有所有线程同时到达这里之后才会继续往下走
+                cyclicBarrier.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            synchronized (key) {
+                System.out.println(name + " " + SystemClock.now() + " ==> 上锁成功，休眠");
+                try {
+                    countDownLatch.countDown();
+                    key.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(name + " " + SystemClock.now() + " ==> 被唤醒");
+                ThreadUtil.sleep(2000);
+            }
+        };
+
+        for (int i = 0; i < threadNum; i++) {
+            Thread t = new Thread(runnable);
+            t.setName("T" + i);
+            t.start();
+        }
+
+
+        try {
+            countDownLatch.await();
+                synchronized (key) {
+                    key.notify();
+                    System.out.println(SystemClock.now() + "唤醒全部");
+                    ThreadUtil.sleep(1000);
+                }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ThreadUtil.sync(new Object());
     }
 
 }
