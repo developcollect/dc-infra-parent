c746a90db66e058ce5ab413ed665dfd9aee9fe32
zak <lanjingl@outlook.com>
2021-10-04 17:50:09 +0800

    树工具类增加单元测试

--- dc-infra-core/src/test/java/com/developcollect/core/tree/TreeUtilTest.java
+++ dc-infra-core/src/test/java/com/developcollect/core/tree/TreeUtilTest.java
@@ -1,16 +1,9 @@
 package com.developcollect.core.tree;
 
-import cn.hutool.core.codec.Base64;
-import cn.hutool.http.HttpUtil;
 import org.junit.Test;
 
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 import static org.junit.Assert.*;
 
@@ -57,6 +50,54 @@ public class TreeUtilTest {
         return tg;
     }
 
+    /**
+     *                      A
+     *      B               C             D
+     * e  f  g  h          ijk           lm
+     *   nz
+     * @return
+     */
+    private TestVO buildTree() {
+        TestVO ta = createTree("A");
+        TestVO tb = createTree("B");
+        TestVO tc = createTree("C");
+        TestVO td = createTree("D");
+        TestVO te = createTree("E");
+        TestVO tf = createTree("F");
+        TestVO tg = createTree("G");
+        TestVO th = createTree("H");
+        TestVO ti = createTree("I");
+        TestVO tj = createTree("J");
+        TestVO tk = createTree("K");
+        TestVO tl = createTree("L");
+        TestVO tm = createTree("M");
+        TestVO tn = createTree("N");
+        TestVO tz = createTree("Z");
+
+        ta.getChildren().add(tb);
+        ta.getChildren().add(tc);
+        ta.getChildren().add(td);
+
+
+        tb.getChildren().add(te);
+        tb.getChildren().add(tf);
+        tb.getChildren().add(tg);
+        tb.getChildren().add(th);
+
+        tc.getChildren().add(ti);
+        tc.getChildren().add(tj);
+        tc.getChildren().add(tk);
+
+        td.getChildren().add(tl);
+        td.getChildren().add(tm);
+
+
+        tf.getChildren().add(tn);
+        tf.getChildren().add(tz);
+
+        return ta;
+    }
+
     private TestVO createTree(String value) {
         TestVO t = new TestVO();
         t.setValue(value);
@@ -69,10 +110,14 @@ public class TreeUtilTest {
     @Test
     public void test_preOrder() {
         TestVO root = buildBinaryTree();
-        StringBuilder sb = new StringBuilder();
-        TreeUtil.preOrder(root, vo -> sb.append(vo.getValue()));
+        StringBuilder sb1 = new StringBuilder();
+        TreeUtil.preOrder(root, vo -> sb1.append(vo.getValue()));
+        assertEquals("GDAFEMHZ", sb1.toString());
 
-        assertEquals("GDAFEMHZ", sb.toString());
+        TestVO t2 = buildTree();
+        StringBuilder sb2 = new StringBuilder();
+        TreeUtil.preOrder(t2, vo -> sb2.append(vo.getValue()));
+        assertEquals("ABEFNZGHCIJKDLM", sb2.toString());
     }
 
     @Test
@@ -80,54 +125,80 @@ public class TreeUtilTest {
         TestVO root = buildBinaryTree();
         StringBuilder sb = new StringBuilder();
         TreeUtil.preOrderRe(root, vo -> sb.append(vo.getValue()));
-
         assertEquals("GDAFEMHZ", sb.toString());
+
+        TestVO t2 = buildTree();
+        StringBuilder sb2 = new StringBuilder();
+        TreeUtil.preOrder(t2, vo -> sb2.append(vo.getValue()));
+        assertEquals("ABEFNZGHCIJKDLM", sb2.toString());
     }
 
     @Test
     public void test_midOrder() {
+        TestVO t2 = buildTree();
+        StringBuilder sb2 = new StringBuilder();
+        TreeUtil.midOrder(t2, vo -> sb2.append(vo.getValue()));
+        assertEquals("EBHALDM", sb2.toString());
+
         TestVO root = buildBinaryTree();
         StringBuilder sb = new StringBuilder();
         TreeUtil.midOrder(root, vo -> sb.append(vo.getValue()));
-
         assertEquals("ADEFGHMZ", sb.toString());
     }
 
     @Test
     public void test_midOrderRe() {
+        TestVO t2 = buildTree();
+        StringBuilder sb2 = new StringBuilder();
+        TreeUtil.midOrderRe(t2, vo -> sb2.append(vo.getValue()));
+        assertEquals("EBHALDM", sb2.toString());
+
         TestVO root = buildBinaryTree();
         StringBuilder sb = new StringBuilder();
         TreeUtil.midOrderRe(root, vo -> sb.append(vo.getValue()));
-
         assertEquals("ADEFGHMZ", sb.toString());
     }
 
     @Test
     public void test_postOrder() {
+        TestVO t2 = buildTree();
+        StringBuilder sb2 = new StringBuilder();
+        TreeUtil.postOrder(t2, vo -> sb2.append(vo.getValue()));
+        assertEquals("ENZFGHBIJKCLMDA", sb2.toString());
+
         TestVO root = buildBinaryTree();
         StringBuilder sb = new StringBuilder();
         TreeUtil.postOrder(root, vo -> sb.append(vo.getValue()));
-
         assertEquals("AEFDHZMG", sb.toString());
     }
 
 
     @Test
     public void test_postOrderRe() {
+        TestVO t2 = buildTree();
+        StringBuilder sb2 = new StringBuilder();
+        TreeUtil.postOrderRe(t2, vo -> sb2.append(vo.getValue()));
+        assertEquals("ENZFGHBIJKCLMDA", sb2.toString());
+
+
         TestVO root = buildBinaryTree();
         StringBuilder sb = new StringBuilder();
         TreeUtil.postOrderRe(root, vo -> sb.append(vo.getValue()));
-
         assertEquals("AEFDHZMG", sb.toString());
     }
 
 
     @Test
     public void test_levelOrderRe() {
+        TestVO t2 = buildTree();
+        StringBuilder sb2 = new StringBuilder();
+        TreeUtil.levelOrder(t2, vo -> sb2.append(vo.getValue()));
+        assertEquals("ABCDEFGHIJKLMNZ", sb2.toString());
+
+
         TestVO root = buildBinaryTree();
         StringBuilder sb = new StringBuilder();
         TreeUtil.levelOrder(root, vo -> sb.append(vo.getValue()));
-
         assertEquals("GDMAFHZE", sb.toString());
     }
 }
