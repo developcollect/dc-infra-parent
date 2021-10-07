59c36fbae0a52987773821e3edac7429490e5067
zak <lanjingl@outlook.com>
2021-10-07 13:09:29 +0800

    ccutil

--- dc-infra-extra/src/main/java/com/developcollect/extra/javacc/CcUtil.java
+++ dc-infra-extra/src/main/java/com/developcollect/extra/javacc/CcUtil.java
@@ -1,8 +1,6 @@
 package com.developcollect.extra.javacc;
 
 
-import com.developcollect.extra.maven.MavenUtil;
-import com.developcollect.extra.maven.ProjectStructure;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -18,29 +16,6 @@ import java.util.stream.Collectors;
 public class CcUtil {
 
 
-    public static Map<ClassAndMethod, CallInfo> parseChain(String mavenProjectDir, Predicate<ClassAndMethod> scanFilter) {
-        return parseChain(mavenProjectDir, scanFilter, null);
-    }
-
-
-    /**
-     * 传入一个Maven项目，解析该项目中的类中的方法调用关系
-     *
-     * @param mavenProjectDir maven项目文件夹
-     * @return
-     */
-    public static Map<ClassAndMethod, CallInfo> parseChain(String mavenProjectDir, Predicate<ClassAndMethod> scanFilter, Predicate<CallInfo> parseFilter) {
-        // 解析maven项目结构
-        ProjectStructure projectStructure = MavenUtil.analysisProject(mavenProjectDir);
-
-        // 执行clear、compile命令，定位classes目录
-        MavenUtil.mvnWithThrow(projectStructure.getPomPath(), "clean", "compile");
-
-        // 定位classes目录
-        String[] classPaths = MavenUtil.collectClassPaths(projectStructure);
-
-        return parseChain(classPaths, scanFilter, parseFilter, null);
-    }
 
     public static Map<ClassAndMethod, CallInfo> parseChain(String[] paths, Predicate<ClassAndMethod> scanFilter, Predicate<CallInfo> parseFilter, CallChainParser.SubClassScanner subClassScanner) {
         // 扫描类，定位需要解析的类和方法
--- dc-infra-extra/src/test/java/com/developcollect/extra/javacc/CcUtilTest.java
+++ dc-infra-extra/src/test/java/com/developcollect/extra/javacc/CcUtilTest.java
@@ -1,11 +1,14 @@
 package com.developcollect.extra.javacc;
 
+import com.developcollect.extra.maven.MavenUtil;
+import com.developcollect.extra.maven.ProjectStructure;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
 import org.apache.bcel.generic.Type;
 import org.junit.Test;
 
 import java.util.Map;
+import java.util.function.Predicate;
 
 public class CcUtilTest {
 
@@ -18,12 +21,12 @@ public class CcUtilTest {
 
     @Test
     public void test2a() {
-        CcUtil.parseChain("/Volumes/D2/code/java-projects/dc-infra-parent", cm -> true);
+        parseChain("/Volumes/D2/code/java-projects/dc-infra-parent", cm -> true);
     }
 
     @Test
     public void test2b() {
-        Map<ClassAndMethod, CallInfo> chainMap = CcUtil.parseChain("/Volumes/D2/code/java-projects/first", cm -> {
+        Map<ClassAndMethod, CallInfo> chainMap = parseChain("/Volumes/D2/code/java-projects/first", cm -> {
             JavaClass javaClass = cm.getJavaClass();
             Method method = cm.getMethod();
             if (javaClass.getClassName().equals("org.example.TestEntry") && method.getName().equals("en2")) {
@@ -41,7 +44,7 @@ public class CcUtilTest {
 
     @Test
     public void testf1() {
-        Map<ClassAndMethod, CallInfo> chainMap = CcUtil.parseChain("/Volumes/D2/code/java-projects/first", cm -> {
+        Map<ClassAndMethod, CallInfo> chainMap = parseChain("/Volumes/D2/code/java-projects/first", cm -> {
             JavaClass javaClass = cm.getJavaClass();
             Method method = cm.getMethod();
             return javaClass.getClassName().equals("org.example.TestEntry") && method.getName().equals("f1");
@@ -53,7 +56,7 @@ public class CcUtilTest {
 
     @Test
     public void test_TestEntry() {
-        Map<ClassAndMethod, CallInfo> chainMap = CcUtil.parseChain("/Volumes/D2/code/java-projects/first", cm -> {
+        Map<ClassAndMethod, CallInfo> chainMap = parseChain("/Volumes/D2/code/java-projects/first", cm -> {
             JavaClass javaClass = cm.getJavaClass();
             return javaClass.getClassName().equals("org.example.TestEntry");
         });
@@ -64,7 +67,7 @@ public class CcUtilTest {
 
     @Test
     public void testf33() {
-        Map<ClassAndMethod, CallInfo> chainMap = CcUtil.parseChain("/Volumes/D2/code/java-projects/first", cm -> {
+        Map<ClassAndMethod, CallInfo> chainMap = parseChain("/Volumes/D2/code/java-projects/first", cm -> {
             JavaClass javaClass = cm.getJavaClass();
             Method method = cm.getMethod();
             return javaClass.getClassName().equals("org.example.TestEntry") && method.getName().equals("f33");
@@ -76,5 +79,30 @@ public class CcUtilTest {
         });
 
         printChainMap(chainMap);
+    }
+
+
+    public static Map<ClassAndMethod, CallInfo> parseChain(String mavenProjectDir, Predicate<ClassAndMethod> scanFilter) {
+        return parseChain(mavenProjectDir, scanFilter, null);
+    }
+
+
+    /**
+     * 传入一个Maven项目，解析该项目中的类中的方法调用关系
+     *
+     * @param mavenProjectDir maven项目文件夹
+     * @return
+     */
+    public static Map<ClassAndMethod, CallInfo> parseChain(String mavenProjectDir, Predicate<ClassAndMethod> scanFilter, Predicate<CallInfo> parseFilter) {
+        // 解析maven项目结构
+        ProjectStructure projectStructure = MavenUtil.analysisProject(mavenProjectDir);
+
+        // 执行clear、compile命令，定位classes目录
+        MavenUtil.mvnWithThrow(projectStructure.getPomPath(), "clean", "compile");
+
+        // 定位classes目录
+        String[] classPaths = MavenUtil.collectClassPaths(projectStructure);
+
+        return CcUtil.parseChain(classPaths, scanFilter, parseFilter, null);
     }
 }
