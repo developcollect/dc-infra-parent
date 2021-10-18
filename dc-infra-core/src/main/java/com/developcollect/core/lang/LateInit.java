b7acaf216c74b914f451dfed2e2ac613aacdb9c4
zak <lanjingl@outlook.com>
2021-10-18 18:18:45 +0800

    1

---
+++ dc-infra-core/src/main/java/com/developcollect/core/lang/LateInit.java
@@ -0,0 +1,30 @@
+package com.developcollect.core.lang;
+
+import java.util.function.Supplier;
+
+/**
+ * 延迟创建对象
+ * @param <T>
+ */
+public class LateInit<T> {
+
+    private final Supplier<T> supplier;
+    private final Object lock = new Object();
+    private volatile T ref;
+
+    public LateInit(Supplier<T> supplier) {
+        this.supplier = supplier;
+    }
+
+    public T get() {
+        if (ref == null) {
+            synchronized (lock) {
+                if (ref == null) {
+                    ref = supplier.get();
+                }
+            }
+        }
+
+        return ref;
+    }
+}
