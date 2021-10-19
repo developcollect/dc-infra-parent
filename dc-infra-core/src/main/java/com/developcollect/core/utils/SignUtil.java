1a84db358b229ac48813ed8fc74e04210973deda
zak <lanjingl@outlook.com>
2021-10-19 22:06:45 +0800

    1

--- dc-infra-core/src/main/java/com/developcollect/core/utils/SignUtil.java
+++ dc-infra-core/src/main/java/com/developcollect/core/utils/SignUtil.java
@@ -5,7 +5,6 @@ import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.Mode;
 import cn.hutool.crypto.Padding;
-import cn.hutool.crypto.SecureUtil;
 import cn.hutool.crypto.asymmetric.KeyType;
 import cn.hutool.crypto.asymmetric.RSA;
 import cn.hutool.crypto.asymmetric.Sign;
@@ -14,116 +13,52 @@ import cn.hutool.crypto.symmetric.AES;
 import com.alibaba.fastjson.JSON;
 
 import java.nio.charset.StandardCharsets;
-import java.security.KeyPair;
 import java.util.*;
+import java.util.function.Function;
 
+
+/**
+ * 报文签名和验签工具类
+ */
 public class SignUtil {
 
 
-
-    public static void main(String[] args) {
-        generateKey();
-
-        signTest();
-
-        verifyTest();
-    }
-
-
-    /**
-     * 签名测试
-     */
-    private static void signTest() {
-        // rsa私钥
-        String rsaPrivateKey = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJu27RTGVu9IJCH9R+yodLLHSH8KkkxV5elm++C2EXZbu5C6CPNqztUhkIbqRTELkDJgR/lhuCKVP05efWOGYtub+TurrCDxuqM06AM03dqz8Om0ia4WSCOjXVPyeabs821yX3Ctme2XT5D4kk4Agn7yr7KgscXJAuajXJuxeOM3AgMBAAECgYAQUq6rnNcEIlXXUku13TrOeuF4n80SP3bobqk8txlBhQA2fanuimXAJdKWsfwq1gF5pmolFD9PMMIAHxuZ0T6PiLp2RINyg5hoYeX3qHLCg4zTtthzlPHYSumo6XX4tAUTzLu5vdUmJfxm2I9iqxJYL1vii6YW4ZtEetJebS2i0QJBAN/FVfr4erKLa5duHsSdvij2DeUId0nuI5N21kwfgQ6/f8k4LnmqL4dmdpjdkLyDgZM0wrtSvYfiAKvJNMrkAqMCQQCyJEizPw0OHIMGZaM2QL8TFzTO0+xLN78L4K8rIE8U6e4KWqocIRR4mbNqlAz+DVOclvZ4wwcSxGg8oXDneTpdAkA9BcN8vWY4amzcztr1I09IPFFts/FT5+0ruayW9cBsFSzz4q5J3282rWqKJWHjBrm/OxQfoWCuPaORNT2AVs2hAkAdkB/wfzovaVPlL+DSFBShmmxNFqZUJUAzPGpVgdsd4WR7m2g3mtXG3dsEiOVPE+8YQYVNrS/zUVzrEu+lulyRAkEAj2nefO1KU+GvKBeqJ5IzNcfWUPZec85GR1GIBNUFiRLKfaiqiuDQEWcnqnYwWPbuzgplLLYo8D6KnkAf3AzvTA==";
-        String ll = "";
-        // 业务字段转JSON
-        Map<String, Object> map = new HashMap<>();
-        map.put("name", "张三");
-        map.put("age", 22);
-        map.put("Avatar", "");
-
-        String bizContentJsonStr = JSON.toJSONString(map);
-
-        Map<String, String> requestMap = signAndEncryptBizContent(bizContentJsonStr, rsaPrivateKey, "");
-
-        // 输出最终请求参数
-        System.out.println("最终请求参数：" + JSON.toJSONString(requestMap));
-    }
-
-    /**
-     * 验签测试
-     */
-    private static void verifyTest() {
-        // rsa公钥
-        String rsaPublicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbtu0UxlbvSCQh/UfsqHSyx0h/CpJMVeXpZvvgthF2W7uQugjzas7VIZCG6kUxC5AyYEf5YbgilT9OXn1jhmLbm/k7q6wg8bqjNOgDNN3as/DptImuFkgjo11T8nmm7PNtcl9wrZntl0+Q+JJOAIJ+8q+yoLHFyQLmo1ybsXjjNwIDAQAB";
-        String originBody = "{\"secretKey\":\"gtkHwM+ZqIWSGzzJaLCEkcRBl2vD1YYmOHGpu/JGVBDII+Ne6FoC8bLb0IDAwFFnZhxHmdBXbqyf/Dg2y0AHK/Ovs7Cx7tf41+/0z4aU1CQ4a5wsrX4Mr/H+/1t5Fzc0BIGA4SQ8rTOK1SBogPyaqKwm+2RSk6UimILWFU/Vs94=\",\"bizContent\":\"vf47niJ4BvmRw8zjqmS+eMUE3OOXYhWEmo5CfbRDM/xEblP4hJZwnoiQPsyoVHOk\",\"sign\":\"a2FX1JqLZaJzT/B+QIxW/xpTF1wqGajPKBC6dQGC67zhZJEzwN0QAEvgFcsbB1NsPnMohEk6Lz96Iv9PQrIZCJ/TPDkNVunrrkWd9v5u209d3Q5DkfxPkZf5jrRGYX9Bww0+Ln/FY+UuaT+++lnefUYgfpqMDilrI773gnN5+Gg=\"}";
-
-        String bizContent = verifySignAndDecryptBizContent(originBody, rsaPublicKey, "");
-        System.out.println("业务报文：" + bizContent);
-    }
-
-    /**
-     * 生成RSA的公钥和私钥
-     */
-    private static void generateKey() {
-
-        System.out.println("己方密钥对");
-        KeyPair keyPair = SecureUtil.generateKeyPair("RSA");
-        System.out.println("私钥：" + Base64.encode(keyPair.getPrivate().getEncoded()));
-        System.out.println("公钥：" + Base64.encode(keyPair.getPublic().getEncoded()));
-
-        System.out.println("对方密钥对");
-        KeyPair keyPair2 = SecureUtil.generateKeyPair("RSA");
-        System.out.println("私钥：" + Base64.encode(keyPair2.getPrivate().getEncoded()));
-        System.out.println("公钥：" + Base64.encode(keyPair2.getPublic().getEncoded()));
-    }
-
-
-    /**
-     * 对报文进行验签并解密出业务报文
-     * @param originalBody 原始报文
-     * @param oppositePublicKey rsa公钥
-     * @return 业务报文
-     */
-    public static String verifySignAndDecryptBizContent(String originalBody, String rsaPrivateKey, String oppositePublicKey) {
-        // 原始报文
-        Map<String, Object> bodyMap = JSON.parseObject(originalBody);
-        // 获取签名，并将sign从map中移除
-        String signStr = (String) bodyMap.remove("sign");
-        if (signStr == null) {
-            throw new RuntimeException("签名不存在");
-        }
-
-        // 生成待签字符串
-        String signContent = buildSignContent(bodyMap);
-        // 使用对方的RSA公钥进行验签
-        Sign sign = new Sign(SignAlgorithm.SHA256withRSA, null, Base64.decode(oppositePublicKey));
-        boolean verify = sign.verify(signContent.getBytes(StandardCharsets.UTF_8), Base64.decode(signStr));
-        if (!verify) {
-            throw new RuntimeException("验证签名失败");
-        }
-
-        // 签名验证成功后则解密业务报文
-        String encryptedAesKey = (String) bodyMap.get("secretKey");
-        // 使用己方的RSA私钥解密出AES秘钥
-        RSA rsa = new RSA(Base64.decode(rsaPrivateKey), null);
-        byte[] aesKeyBytes = rsa.decrypt(Base64.decode(encryptedAesKey), KeyType.PrivateKey);
-
-        String encryptedBizContentStr = (String) bodyMap.get("bizContent");
-        AES aes = new AES(Mode.ECB, Padding.PKCS5Padding, aesKeyBytes);
-        String bizContentStr = aes.decryptStr(Base64.decode(encryptedBizContentStr));
-        return bizContentStr;
-    }
-
     /**
      * 对业务报文进行签名并加密
-     * @param bizContent 业务报文
-     * @param rsaPrivateKey 己方的rsa私钥
+     * 使用对方的公钥加密AES秘钥
+     *
+     * @param bizContent        业务报文
+     * @param rsaPrivateKey     己方的rsa私钥
      * @param oppositePublicKey 对方的rsa公钥
      * @return 返回加密后的业务报文以及aes秘钥和签名
      */
     public static Map<String, String> signAndEncryptBizContent(String bizContent, String rsaPrivateKey, String oppositePublicKey) {
+        // 使用对方的RSA公钥对AES秘钥进行加密
+        return doSignAndEncryptBizContent(bizContent, Base64.decode(rsaPrivateKey), aesKeyBytes -> {
+            RSA rsa = new RSA(null, Base64.decode(oppositePublicKey));
+            return rsa.encryptBase64(aesKeyBytes, KeyType.PublicKey);
+        });
+    }
+
+    /**
+     * 对业务报文进行签名并加密
+     * 使用己方的私钥加密AES秘钥
+     *
+     * @param bizContent    业务报文
+     * @param rsaPrivateKey 己方的rsa私钥
+     * @return 返回加密后的业务报文以及aes秘钥和签名
+     */
+    public static Map<String, String> signAndEncryptBizContent(String bizContent, String rsaPrivateKey) {
+        byte[] rsaPrivateKeyBytes = Base64.decode(rsaPrivateKey);
+
+        // 使用己方的RSA私钥加密AES秘钥
+        return doSignAndEncryptBizContent(bizContent, rsaPrivateKeyBytes, aesKeyBytes -> {
+            RSA rsa = new RSA(rsaPrivateKeyBytes, null);
+            return rsa.encryptBase64(aesKeyBytes, KeyType.PrivateKey);
+        });
+    }
+
+    private static Map<String, String> doSignAndEncryptBizContent(String bizContent, byte[] rsaPrivateKeyBytes, Function<byte[], String> aesKeyEncryptor) {
         byte[] bizContentBytes = bizContent.getBytes(StandardCharsets.UTF_8);
 
         // 随机生成AES秘钥
@@ -134,9 +69,8 @@ public class SignUtil {
         AES aes = new AES(Mode.ECB, Padding.PKCS5Padding, aesKeyBytes);
         String encryptedBizContentStr = aes.encryptBase64(bizContentBytes);
 
-        // 使用对方的RSA公钥对AES秘钥进行加密
-        RSA rsa = new RSA(null, Base64.decode(oppositePublicKey));
-        String encryptedAesKey = rsa.encryptBase64(aesKeyBytes, KeyType.PublicKey);
+        // 对AES秘钥进行加密
+        String encryptedAesKey = aesKeyEncryptor.apply(aesKeyBytes);
 
         // 构建公共请求参数
         Map<String, String> requestParams = new HashMap<>();
@@ -144,7 +78,7 @@ public class SignUtil {
         requestParams.put("secretKey", encryptedAesKey);
 
         // 对公共请求参数进行签名
-        Sign sign = new Sign(SignAlgorithm.SHA256withRSA, Base64.decode(rsaPrivateKey), null);
+        Sign sign = new Sign(SignAlgorithm.SHA256withRSA, rsaPrivateKeyBytes, null);
         // 生成待签字符串
         String signContent = buildSignContent(requestParams);
         // 执行签名
@@ -157,8 +91,77 @@ public class SignUtil {
         return requestParams;
     }
 
+
+    /**
+     * 对报文进行验签并解密出业务报文
+     * 使用己方的rsa私钥解密AES秘钥
+     *
+     * @param originalBody      原始报文
+     * @param rsaPrivateKey     己方的rsa私钥
+     * @param oppositePublicKey 对方的rsa公钥
+     * @return 业务报文
+     */
+    public static String verifySignAndDecryptBizContent(String originalBody, String rsaPrivateKey, String oppositePublicKey) {
+        byte[] oppositePublicKeyBytes = Base64.decode(oppositePublicKey);
+        // 使用己方的私钥解密AES秘钥
+        return doVerifySignAndDecryptBizContent(originalBody, oppositePublicKeyBytes, encryptedAesKey -> {
+            RSA rsa = new RSA(Base64.decode(rsaPrivateKey), null);
+            return rsa.decrypt(encryptedAesKey, KeyType.PrivateKey);
+        });
+    }
+
+    /**
+     * 对报文进行验签并解密出业务报文
+     * 使用对方的rsa公钥解密AES秘钥
+     *
+     * @param originalBody      原始报文
+     * @param oppositePublicKey 对方的rsa公钥
+     * @return 业务报文
+     */
+    public static String verifySignAndDecryptBizContent(String originalBody, String oppositePublicKey) {
+        byte[] oppositePublicKeyBytes = Base64.decode(oppositePublicKey);
+        // 使用对方的公钥解密AES秘钥
+        return doVerifySignAndDecryptBizContent(originalBody, oppositePublicKeyBytes, encryptedAesKey -> {
+            RSA rsa = new RSA(null, oppositePublicKeyBytes);
+            return rsa.decrypt(encryptedAesKey, KeyType.PublicKey);
+        });
+    }
+
+
+    private static String doVerifySignAndDecryptBizContent(String originalBody, byte[] oppositePublicKeyBytes, Function<String, byte[]> aesKeyDecryptor) {
+        // 原始报文
+        Map<String, Object> bodyMap = JSON.parseObject(originalBody);
+        // 获取签名，并将sign从map中移除
+        String signStr = (String) bodyMap.remove("sign");
+        if (signStr == null) {
+            throw new RuntimeException("签名不存在");
+        }
+
+
+        // 生成待签字符串
+        String signContent = buildSignContent(bodyMap);
+        // 使用对方的RSA公钥进行验签
+        Sign sign = new Sign(SignAlgorithm.SHA256withRSA, null, oppositePublicKeyBytes);
+        boolean verify = sign.verify(signContent.getBytes(StandardCharsets.UTF_8), Base64.decode(signStr));
+        if (!verify) {
+            throw new RuntimeException("验证签名失败");
+        }
+
+        // 签名验证成功后则解密业务报文
+        String encryptedAesKey = (String) bodyMap.get("secretKey");
+        // 解密出AES秘钥
+        byte[] aesKeyBytes = aesKeyDecryptor.apply(encryptedAesKey);
+
+        String encryptedBizContentStr = (String) bodyMap.get("bizContent");
+        AES aes = new AES(Mode.ECB, Padding.PKCS5Padding, aesKeyBytes);
+        String bizContentStr = aes.decryptStr(Base64.decode(encryptedBizContentStr));
+        return bizContentStr;
+    }
+
+
     /**
      * 构建待签名字符串
+     *
      * @param paramMap 参数map
      * @return 待签名字符串
      */
@@ -188,8 +191,6 @@ public class SignUtil {
         }
         return content.toString();
     }
-
-
 
 
 }
--- dc-infra-core/src/test/java/com/developcollect/core/utils/SignUtilTest.java
+++ dc-infra-core/src/test/java/com/developcollect/core/utils/SignUtilTest.java
@@ -1,52 +1,103 @@
 package com.developcollect.core.utils;
 
+import cn.hutool.core.codec.Base64;
+import cn.hutool.crypto.SecureUtil;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
 
+import java.security.KeyPair;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class SignUtilTest {
 
-    public static void main(String[] args) {
-        signTest();
 
-        verifyTest();
+    /**
+     * 只使用一对秘钥进行验签和加密
+     */
+    @Test
+    public void test_sign_v() {
+        // rsa私钥
+        String rsaPrivateKey = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJu27RTGVu9IJCH9R+yodLLHSH8KkkxV5elm++C2EXZbu5C6CPNqztUhkIbqRTELkDJgR/lhuCKVP05efWOGYtub+TurrCDxuqM06AM03dqz8Om0ia4WSCOjXVPyeabs821yX3Ctme2XT5D4kk4Agn7yr7KgscXJAuajXJuxeOM3AgMBAAECgYAQUq6rnNcEIlXXUku13TrOeuF4n80SP3bobqk8txlBhQA2fanuimXAJdKWsfwq1gF5pmolFD9PMMIAHxuZ0T6PiLp2RINyg5hoYeX3qHLCg4zTtthzlPHYSumo6XX4tAUTzLu5vdUmJfxm2I9iqxJYL1vii6YW4ZtEetJebS2i0QJBAN/FVfr4erKLa5duHsSdvij2DeUId0nuI5N21kwfgQ6/f8k4LnmqL4dmdpjdkLyDgZM0wrtSvYfiAKvJNMrkAqMCQQCyJEizPw0OHIMGZaM2QL8TFzTO0+xLN78L4K8rIE8U6e4KWqocIRR4mbNqlAz+DVOclvZ4wwcSxGg8oXDneTpdAkA9BcN8vWY4amzcztr1I09IPFFts/FT5+0ruayW9cBsFSzz4q5J3282rWqKJWHjBrm/OxQfoWCuPaORNT2AVs2hAkAdkB/wfzovaVPlL+DSFBShmmxNFqZUJUAzPGpVgdsd4WR7m2g3mtXG3dsEiOVPE+8YQYVNrS/zUVzrEu+lulyRAkEAj2nefO1KU+GvKBeqJ5IzNcfWUPZec85GR1GIBNUFiRLKfaiqiuDQEWcnqnYwWPbuzgplLLYo8D6KnkAf3AzvTA==";
+        // rsa公钥
+        String rsaPublicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbtu0UxlbvSCQh/UfsqHSyx0h/CpJMVeXpZvvgthF2W7uQugjzas7VIZCG6kUxC5AyYEf5YbgilT9OXn1jhmLbm/k7q6wg8bqjNOgDNN3as/DptImuFkgjo11T8nmm7PNtcl9wrZntl0+Q+JJOAIJ+8q+yoLHFyQLmo1ybsXjjNwIDAQAB";
+
+
+        // 业务字段转JSON
+        String bizContentJsonStr = JSON.toJSONString(getBizContentMap());
+        // 加签
+        Map<String, String> requestMap = SignUtil.signAndEncryptBizContent(bizContentJsonStr, rsaPrivateKey);
+        String signedBody = JSON.toJSONString(requestMap);
+        // 输出最终请求参数
+        System.out.println("最终请求参数：" + signedBody);
+
+        String bizContent = SignUtil.verifySignAndDecryptBizContent(signedBody, rsaPublicKey);
+        System.out.println("业务报文：" + bizContent);
+        JSONObject bizContentMap = JSON.parseObject(bizContent);
+        assertBizContentMap(bizContentMap);
     }
 
 
     /**
-     * 签名测试
+     * 使用两对秘钥进行验签和加密
      */
-    private static void signTest() {
-        // rsa私钥
-        String rsaPrivateKey = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJu27RTGVu9IJCH9R+yodLLHSH8KkkxV5elm++C2EXZbu5C6CPNqztUhkIbqRTELkDJgR/lhuCKVP05efWOGYtub+TurrCDxuqM06AM03dqz8Om0ia4WSCOjXVPyeabs821yX3Ctme2XT5D4kk4Agn7yr7KgscXJAuajXJuxeOM3AgMBAAECgYAQUq6rnNcEIlXXUku13TrOeuF4n80SP3bobqk8txlBhQA2fanuimXAJdKWsfwq1gF5pmolFD9PMMIAHxuZ0T6PiLp2RINyg5hoYeX3qHLCg4zTtthzlPHYSumo6XX4tAUTzLu5vdUmJfxm2I9iqxJYL1vii6YW4ZtEetJebS2i0QJBAN/FVfr4erKLa5duHsSdvij2DeUId0nuI5N21kwfgQ6/f8k4LnmqL4dmdpjdkLyDgZM0wrtSvYfiAKvJNMrkAqMCQQCyJEizPw0OHIMGZaM2QL8TFzTO0+xLN78L4K8rIE8U6e4KWqocIRR4mbNqlAz+DVOclvZ4wwcSxGg8oXDneTpdAkA9BcN8vWY4amzcztr1I09IPFFts/FT5+0ruayW9cBsFSzz4q5J3282rWqKJWHjBrm/OxQfoWCuPaORNT2AVs2hAkAdkB/wfzovaVPlL+DSFBShmmxNFqZUJUAzPGpVgdsd4WR7m2g3mtXG3dsEiOVPE+8YQYVNrS/zUVzrEu+lulyRAkEAj2nefO1KU+GvKBeqJ5IzNcfWUPZec85GR1GIBNUFiRLKfaiqiuDQEWcnqnYwWPbuzgplLLYo8D6KnkAf3AzvTA==";
+    @Test
+    public void test_sign_v2() {
+        // 己方秘钥对
+        String rsaPrivateKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBALnkTriEDvzR5LchtX0RQ3UXRzhT5B4u6uu/wQ532CZtzUHfDZqcJ6FKriSJ8b284Mcoyt7sNE+jA4CLhxgm0q09I7KC9xOaK84oZH7stVfNhT4Syn7B9FgarAAmwoy+zUIY0Amv5iE9EeMdwgDlgXggvMfp7cPznzjn6jMv3GszAgMBAAECgYBTT9toHueIr5lN6jGox9/vzmsA97g5PtXSqrIfhd69+mAPsaQrMukgBJRfk+NgIhKrr8CcgEhBmNGs/tuhG2U3sas6WoP78dEQsDTgWbTTDx7lhLwDbbZVAbdLPpp4jep2XyKcjjln8fvfM8xHkhoIxMgJ/3PK+hmSE/F8Uiq4YQJBAPvdGaEcbQEQ2IGuL5KmJPPz1hltj21gmaCuMU17/SrEVprs06rvlBCUI5I/E3k9kg2xSU2AO0B9j0/Iv/xG5X8CQQC88dg/i7vrKhF+62/AgwrK6I70S2W15k+lbaP8Q2VJMTgbY/+FYbyXzTDjTSOdRNCk1eK+DiCU+Fkd8G4dUJxNAkBsxzx3ggBuMmlf8OwpCUrydgClMNV+ZvdTF4jBr2hTXUMOXHS64ghv9Js8YbO2bGo1Mbm4GEb+/nuz+L86ZSBfAkEAk2UCskfal9Bgh6c4rXBMmg+jfVXYCXeaSE3osCEPQXLA9zUiHzYofi9W5OrHTqdhMEWeNW8l4bB/LMjjzcCVvQJBAK9qgDzdOmTM1hAsRtaHERt2GFwqj0IBQEhYcwn6ed2wEkkRqZRK639b/KT13yN5jYTfCPNN+VKNkWvDsH1qrAE=";
+        String rsaPublicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC55E64hA780eS3IbV9EUN1F0c4U+QeLurrv8EOd9gmbc1B3w2anCehSq4kifG9vODHKMre7DRPowOAi4cYJtKtPSOygvcTmivOKGR+7LVXzYU+Esp+wfRYGqwAJsKMvs1CGNAJr+YhPRHjHcIA5YF4ILzH6e3D85845+ozL9xrMwIDAQAB";
+        // 对方秘钥对
+        String oppositePrivateKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAJ+pvbzyJzQwEL8QrD/SfokHUqXSvW9GGSxNGierTbsg1LRjzu/Nf1iWXVHMcpHn07KEdW++IGDwuI4kFUOxHYrXf+N3CtTvzf/nmzybdP8cCtJMLEqpxFDWrxqIZHuoUZpQPyTfo3QqrJkFG+A5L1XjpMwI0xPY3ouYZbqwtMwNAgMBAAECgYBeTc7A7DJVs9IgWEO1CqosW4PoZ7n2IGw762p4ZcjqrzCTP6R5pWIDPATwjJZbMZeriGsozh6nw2bupcddjR6J9LN86/iCxQzmp2ipNYtzJLgo+JlYW6Ss/rmji4s++aMSTAoIKx8Fv5FJx8Jp0ztgVN91MlNfXRXmBM6xGoxBbQJBANtHXaYhkErbzmYN40PNrAzPwGil5ur1g4933qJFAJGWF5KM7aI1gTzCYvUGEqb24xoStu4jCIJPbjWTmuz51qsCQQC6ZpuT8F9pbw6oYl01vEjXMWcvG6RpZsm1/KFjQDUUYLVfchqHq7ndV2I6JTpzYyQeZxnmgqo0D8JH9Xk6z0gnAkEA1UXeQf0XGJgdcYlEZ+gc1QEjhSUHKbPNV7cg5Pb0DKXNG47SehhMMMpn8SUM8zPhwrQ9E/UjC4syCA4eeyqsrQJBALUP7pfpqgSUaDyQL9J+rpK53m0YAMCRo+YvG+V4/RZqSkQIZlTbLpQeyb+JYhmrd+5AYBxVWFdx51pWzAegSYkCQD3dPHrxesf0QNKrQyNAhXdW3d4Wd6KYH99mRp/ExnbbPHqwafSFOAdOnq8UZglQFLQq+wl2jd/3Vhhk+hDLmMc=";
+        String oppositePublicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCfqb288ic0MBC/EKw/0n6JB1Kl0r1vRhksTRonq027INS0Y87vzX9Yll1RzHKR59OyhHVvviBg8LiOJBVDsR2K13/jdwrU783/55s8m3T/HArSTCxKqcRQ1q8aiGR7qFGaUD8k36N0KqyZBRvgOS9V46TMCNMT2N6LmGW6sLTMDQIDAQAB";
+
 
         // 业务字段转JSON
+        String bizContentJsonStr = JSON.toJSONString(getBizContentMap());
+        // 加签并加密
+        Map<String, String> requestMap = SignUtil.signAndEncryptBizContent(bizContentJsonStr, rsaPrivateKey, oppositePublicKey);
+        String signedBody = JSON.toJSONString(requestMap);
+        // 输出最终请求参数
+        System.out.println("最终请求参数：" + signedBody);
+
+        // 验签并解密
+        String bizContent = SignUtil.verifySignAndDecryptBizContent(signedBody, oppositePrivateKey, rsaPublicKey);
+        System.out.println("业务报文：" + bizContent);
+        JSONObject bizContentMap = JSON.parseObject(bizContent);
+        assertBizContentMap(bizContentMap);
+    }
+
+
+    @Test
+    public void genKey() {
+        KeyPair keyPair = SecureUtil.generateKeyPair("RSA");
+        System.out.println("己方秘钥对");
+        System.out.println("私钥：" + Base64.encode(keyPair.getPrivate().getEncoded()));
+        System.out.println("公钥：" + Base64.encode(keyPair.getPublic().getEncoded()));
+
+        KeyPair keyPair2 = SecureUtil.generateKeyPair("RSA");
+        System.out.println("对方秘钥对");
+        System.out.println("私钥：" + Base64.encode(keyPair2.getPrivate().getEncoded()));
+        System.out.println("公钥：" + Base64.encode(keyPair2.getPublic().getEncoded()));
+    }
+
+
+    private Map<String, Object> getBizContentMap() {
         Map<String, Object> map = new HashMap<>();
         map.put("name", "张三");
         map.put("age", 22);
         map.put("Avatar", "");
-
-        String bizContentJsonStr = JSON.toJSONString(map);
-
-        Map<String, String> requestMap = SignUtil.signAndEncryptBizContent(bizContentJsonStr, rsaPrivateKey);
-
-        // 输出最终请求参数
-        System.out.println("最终请求参数：" + JSON.toJSONString(requestMap));
+        return map;
     }
 
-    /**
-     * 验签测试
-     */
-    private static void verifyTest() {
-        // rsa公钥
-        String rsaPublicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbtu0UxlbvSCQh/UfsqHSyx0h/CpJMVeXpZvvgthF2W7uQugjzas7VIZCG6kUxC5AyYEf5YbgilT9OXn1jhmLbm/k7q6wg8bqjNOgDNN3as/DptImuFkgjo11T8nmm7PNtcl9wrZntl0+Q+JJOAIJ+8q+yoLHFyQLmo1ybsXjjNwIDAQAB";
-        String originBody = "{\"secretKey\":\"gtkHwM+ZqIWSGzzJaLCEkcRBl2vD1YYmOHGpu/JGVBDII+Ne6FoC8bLb0IDAwFFnZhxHmdBXbqyf/Dg2y0AHK/Ovs7Cx7tf41+/0z4aU1CQ4a5wsrX4Mr/H+/1t5Fzc0BIGA4SQ8rTOK1SBogPyaqKwm+2RSk6UimILWFU/Vs94=\",\"bizContent\":\"vf47niJ4BvmRw8zjqmS+eMUE3OOXYhWEmo5CfbRDM/xEblP4hJZwnoiQPsyoVHOk\",\"sign\":\"a2FX1JqLZaJzT/B+QIxW/xpTF1wqGajPKBC6dQGC67zhZJEzwN0QAEvgFcsbB1NsPnMohEk6Lz96Iv9PQrIZCJ/TPDkNVunrrkWd9v5u209d3Q5DkfxPkZf5jrRGYX9Bww0+Ln/FY+UuaT+++lnefUYgfpqMDilrI773gnN5+Gg=\"}";
-
-        String bizContent = SignUtil.verifySignAndDecryptBizContent(originBody, rsaPublicKey);
-        System.out.println("业务报文：" + bizContent);
+    private void assertBizContentMap(Map<String, Object> bizContentMap) {
+        assertEquals(3, bizContentMap.size());
+        assertEquals("张三", bizContentMap.get("name"));
+        assertEquals(22, bizContentMap.get("age"));
+        assertEquals("", bizContentMap.get("Avatar"));
     }
+
 
 }
