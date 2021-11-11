a0a5117c3a1245b363ebad1b458fcbd4dbb67cbe
zak <lanjingl@outlook.com>
2021-11-11 10:24:22 +0800

    1

--- dc-infra-core/src/main/java/com/developcollect/core/thread/lock/CacheLock.java
+++ dc-infra-core/src/main/java/com/developcollect/core/thread/lock/CacheLock.java
@@ -15,5 +15,41 @@ public interface CacheLock extends Lock {
      */
     String getKey();
 
+    /**
+     * 强制释放锁
+     *
+     * @return 如果锁存在并且已经释放则返回true，否则返回false
+     */
+    boolean forceUnlock();
+
+    /**
+     * 检查此锁是否被任意线程锁定
+     *
+     * @return <code>true</code> if locked otherwise <code>false</code>
+     */
+    boolean isLocked();
+
+    /**
+     * 检查指定线程是否持有此锁
+     *
+     * @param threadId 线程id
+     * @return 如果由指定线程持有返回true，否则返回false
+     */
+    boolean isHeldByThread(long threadId);
+
+    /**
+     * 检查当前线程是否持有此锁
+     *
+     * @return 如果由当前线程持有返回true，否则返回false
+     */
+    boolean isHeldByCurrentThread();
+
+    /**
+     * 当前线程持有该锁的次数，如果当前线程不持有该锁则返回0
+     *
+     * @return 当前线程持有该锁的次数，如果当前线程不持有该锁则返回0
+     */
+    int getHoldCount();
+
 
 }
