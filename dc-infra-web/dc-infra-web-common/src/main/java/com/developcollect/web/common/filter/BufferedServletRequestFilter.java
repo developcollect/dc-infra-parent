0ca8b291d9a66830b1bcfcd20e40a8a577e21942
zak <lanjingl@outlook.com>
2021-11-14 22:51:41 +0800

    1

--- dc-infra-web/dc-infra-web-common/src/main/java/com/developcollect/web/common/filter/BufferedServletRequestFilter.java
+++ dc-infra-web/dc-infra-web-common/src/main/java/com/developcollect/web/common/filter/BufferedServletRequestFilter.java
@@ -1,5 +1,6 @@
 package com.developcollect.web.common.filter;
 
+import com.developcollect.extra.servlet.ServletUtil;
 import com.developcollect.web.common.http.wrapper.MutableBufferedServletRequestWrapper;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,7 +18,9 @@ public class BufferedServletRequestFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest requestRef = (HttpServletRequest) request;
 
-        if (!"GET".equals(requestRef.getMethod()) && !"HEAD".equals(requestRef.getMethod())) {
+        if (!"GET".equals(requestRef.getMethod())
+                && !"HEAD".equals(requestRef.getMethod())
+                && !ServletUtil.isMultipart(requestRef)) {
             try {
                 requestRef = new MutableBufferedServletRequestWrapper((HttpServletRequest) request);
             } catch (Exception e) {
