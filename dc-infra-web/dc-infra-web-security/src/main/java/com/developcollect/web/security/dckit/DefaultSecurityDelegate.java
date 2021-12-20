8b5f7c5a814bb419781211c2f1556f40e6bad11a
zak <lanjingl@outlook.com>
2021-12-20 23:22:01 +0800

    1

--- dc-infra-web/dc-infra-web-security/src/main/java/com/developcollect/web/security/dckit/DefaultSecurityDelegate.java
+++ dc-infra-web/dc-infra-web-security/src/main/java/com/developcollect/web/security/dckit/DefaultSecurityDelegate.java
@@ -2,24 +2,33 @@ package com.developcollect.web.security.dckit;
 
 import com.developcollect.web.common.security.DcSecurityUser;
 import com.developcollect.web.common.security.SecurityDelegate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 
 import java.util.Optional;
 
+@Slf4j
 public class DefaultSecurityDelegate implements SecurityDelegate {
 
 
     @Override
     public DcSecurityUser getUserDetail() {
         SecurityContext context = SecurityContextHolder.getContext();
-        Object principal = Optional
-                .ofNullable(context)
-                .map(SecurityContext::getAuthentication)
-                .map(Authentication::getPrincipal)
-                .orElse(null);
-        return (DcSecurityUser) principal;
+        if (context == null) {
+            return null;
+        }
+        Authentication authentication = context.getAuthentication();
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof DcSecurityUser) {
+            return (DcSecurityUser) principal;
+        } else {
+            log.debug("Authentication：[{}]", authentication);
+        }
+
+        return null;
     }
 
 }
