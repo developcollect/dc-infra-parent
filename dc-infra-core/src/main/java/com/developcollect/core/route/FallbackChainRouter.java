173733f5b44ccaf7128ec9f4dfb5f89cf8474fd0
zak <lanjingl@outlook.com>
2021-12-30 14:52:47 +0800

    LoopSupplier

---
+++ dc-infra-core/src/main/java/com/developcollect/core/route/FallbackChainRouter.java
@@ -0,0 +1,18 @@
+//package com.developcollect.core.route;
+//
+//import java.util.List;
+//
+//public class FallbackChainRouter<P, O> implements Router<P, List<O>> {
+//
+//    protected List<O> elements;
+//
+//    @Override
+//    public List<O> route(P p) {
+//        return null;
+//    }
+//
+//    private O routeHead() {
+//        return null;
+//    }
+//
+//}
---
+++ dc-infra-core/src/main/java/com/developcollect/core/route/WeightRouter.java
@@ -0,0 +1,16 @@
+package com.developcollect.core.route;
+
+import java.util.List;
+
+/**
+ * 按权重路由
+ * 支持顺序和乱序
+ */
+public class WeightRouter {
+
+
+    public WeightRouter() {
+        // todo
+    }
+
+}
