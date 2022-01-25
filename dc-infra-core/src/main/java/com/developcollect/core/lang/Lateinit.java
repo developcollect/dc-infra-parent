899bcd5682e3afbe62abca330595213cf4ea3985
zak <lanjingl@outlook.com>
2022-01-25 15:30:42 +0800

    1

--- dc-infra-core/src/main/java/com/developcollect/core/lang/Lateinit.java
+++ dc-infra-core/src/main/java/com/developcollect/core/lang/Lateinit.java
@@ -6,13 +6,13 @@ import java.util.function.Supplier;
  * 延迟创建对象
  * @param <T>
  */
-public class LateInit<T> implements AutoCloseable {
+public class Lateinit<T> implements AutoCloseable {
 
     private final Supplier<T> supplier;
     private final Object lock = new Object();
     private volatile T ref;
 
-    public LateInit(Supplier<T> supplier) {
+    public Lateinit(Supplier<T> supplier) {
         this.supplier = supplier;
     }
 
